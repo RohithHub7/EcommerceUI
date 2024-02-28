6e7cebdac805793370ada70aafdea44b39abb075
rohithkumar1080 <rohithkumar1080@gmail.com>
2024-02-28 08:59:30 +0530

    Added 'Product' and 'Suggested Products' componeents

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-product',
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class ProductComponent {
 
+
+  @Input() view: 'grid'|'list' ='grid'
 }
